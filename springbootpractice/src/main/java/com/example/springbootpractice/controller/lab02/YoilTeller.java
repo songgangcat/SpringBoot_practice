24f7ed080c1492e7bcf33f5b306d6364f0008c4d
Seong Gyeol Kim <ljhkys0130@gmail.com>
2026-02-27 01:37:30 +0000

    getYoil

--- springbootpractice/src/main/java/com/example/springbootpractice/controller/lab02/YoilTeller.java
+++ springbootpractice/src/main/java/com/example/springbootpractice/controller/lab02/YoilTeller.java
@@ -1,5 +1,28 @@
 package com.example.springbootpractice.controller.lab02;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+//년월일을 입력하면 요일을 알려주는 원격 프로그램
+@RestController // 등록
+@RequestMapping("/lab02")
 public class YoilTeller {
+
+    @RequestMapping("/getYoil")
+    public String main(HttpServletRequest request) {
+        String year = request.getParameter("year");
+        String month = request.getParameter("month");
+        String day = request.getParameter("day");
+
+        System.out.println("year = "+year);
+        System.out.println("month = "+month);
+        System.out.println("day = "+day);
+
+
+        return "Monday";
+    }
     
 }
